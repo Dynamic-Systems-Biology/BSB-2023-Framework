9e21d7d58bfdcd8475714ba0e0df1cf02bb42033
MarcB1337 <marcelobatista1337@gmail.com>
2023-08-21 06:14:42 -0300

    Add files via upload

--- fwmodule.jl
+++ fwmodule.jl
@@ -23,6 +23,8 @@ module FWmodule
     Pkg.add("ModelingToolkit")
     Pkg.add("Catalyst")
     Pkg.add("Latexify")
+    Pkg.add("DynamicHMC")
+    Pkg.add("Turing")
 
     using DifferentialEquations
     using DiffEqFlux
@@ -43,6 +45,8 @@ module FWmodule
     using Optimization, OptimizationOptimisers, OptimizationOptimJL
     using SBMLToolkit, ModelingToolkit
     using Catalyst, Latexify
+    using DynamicHMC
+    using Turing
     
     function import_sbml(filepath, lvl, version)
         SBMLToolkit.checksupport_file(filepath) 
@@ -98,4 +102,64 @@ module FWmodule
         return Inf;
     end
     ###
+    
+    ###
+
+    # Calculate MAE metric
+    function calculate_mae(simulated, observed)
+        return sum(abs.(simulated - observed)) / length(observed)
+    end
+
+    # Calculate R² metric
+    function calculate_r2(simulated, observed)
+        mean_observed = mean(observed)
+        ss_total = sum((observed .- mean_observed).^2)
+        ss_residual = sum((observed .- simulated).^2)
+        r2 = 1.0 - ss_residual / ss_total
+        return r2
+    end
+
+    # Calculate BIC metric
+    function calculate_bic(simulated, observed, num_parameters, num_data_points)
+        n = num_data_points
+        k = num_parameters
+        residual_sum_of_squares = sum((observed .- simulated).^2)
+        bic = n * log(residual_sum_of_squares / n) + k * log(n)
+        return bic
+    end
+    # Calculate combined score
+    function calculate_combined_score(mae, r2, bic)
+        # You can define your own combined scoring function here
+        # For simplicity, let's use a weighted sum of the three metrics
+        w_mae = 1.0
+        w_r2 = 1.0
+        w_bic = 1.0
+        combined_score = w_mae * mae + w_r2 * r2 - w_bic * bic
+        return combined_score
+    end
+    #
+    function calculate_metrics(sol_nn1, sol_nn2, X, nn_p, model_param)
+    # Calculate metrics for each SBML model
+        mae_nn = calculate_mae(Array(sol_nn1), X)
+        mae_nn2 = calculate_mae(Array(sol_nn2), X)
+        r2_nn = calculate_r2(Array(sol_nn1), X)
+        r2_nn2 = calculate_r2(Array(sol_nn2), X)  
+        num_parameters_nn = length(nn_p)
+        num_parameters_nn2 = length(model_param)
+        num_data_points = length(X)
+        bic_nn = calculate_bic(Array(sol_nn1), X, num_parameters_nn, num_data_points)
+        bic_nn2 = calculate_bic(Array(sol_nn2), X, num_parameters_nn2, num_data_points)
+
+        # Calculate combined scores
+        combined_score_nn = calculate_combined_score(mae_nn, r2_nn, bic_nn)
+        combined_score_nn2 = calculate_combined_score(mae_nn2, r2_nn2, bic_nn2)
+
+        # Compare the results and choose the preferable model based on combined scores
+        if combined_score_nn > combined_score_nn2
+            return "m1"
+        else
+            return "m2"
+        end
+    end
+    
 end
